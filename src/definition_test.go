c4a7b9863d9d23df011ac1ba228982c45d0c90d0
Elias <github@ielaajez.com>
2024-01-24 11:35:59 +0100

    chore: added Makefile according to spec

--- src/definition_test.go
+++ src/definition_test.go
@@ -3,12 +3,11 @@ package servicerunner
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 // This YAML file should be parsed correctly
 func TestValidYaml(t *testing.T) {
-	yamlPath := "../tests/valid.yaml"
+	yamlPath := "../testfiles/valid.yaml"
 	serviceDefinition, err := parseServiceDefinitionFromYaml(yamlPath)
 	if err != nil {
 		t.Errorf("Error parsing yaml: %s", err)
@@ -19,7 +18,7 @@ func TestValidYaml(t *testing.T) {
 
 // This YAML file should error
 func TestInvalidPath(t *testing.T) {
-	yamlPath := "../tests/doesnotexist.yaml"
+	yamlPath := "../testfiles/doesnotexist.yaml"
 	_, err := parseServiceDefinitionFromYaml(yamlPath)
 	if err == nil {
 		t.Errorf("Parsed invalid path")
@@ -27,7 +26,7 @@ func TestInvalidPath(t *testing.T) {
 }
 
 func TestInvalidYamlWithValuesMissing(t *testing.T) {
-	yamlPath := "../tests/invalid-missing.yaml"
+	yamlPath := "../testfiles/invalid-missing.yaml"
 	_, err := parseServiceDefinitionFromYaml(yamlPath)
 	if err == nil {
 		t.Errorf("Parsed invalid yaml")
@@ -35,7 +34,7 @@ func TestInvalidYamlWithValuesMissing(t *testing.T) {
 }
 
 func TestInvalidYamlWithDuplicateNames(t *testing.T) {
-	yamlPath := "../tests/invalid-duplicate-names.yaml"
+	yamlPath := "../testfiles/invalid-duplicate-names.yaml"
 	_, err := parseServiceDefinitionFromYaml(yamlPath)
 	if err == nil {
 		t.Errorf("Parsed invalid yaml")
@@ -43,54 +42,9 @@ func TestInvalidYamlWithDuplicateNames(t *testing.T) {
 }
 
 func TestInvalidYamlWithDuplicateAddresses(t *testing.T) {
-	yamlPath := "../tests/invalid-duplicate-addresses.yaml"
+	yamlPath := "../testfiles/invalid-duplicate-addresses.yaml"
 	_, err := parseServiceDefinitionFromYaml(yamlPath)
 	if err == nil {
 		t.Errorf("Parsed invalid yaml")
 	}
-}
-
-func TestBasicRegistration(t *testing.T) {
-	sysmanDetails := SystemManagerDetails{
-		serverAddress:    "tcp://localhost:1337",
-		publisherAddress: "tcp://localhost:1338",
-	}
-
-	done := make(chan bool)
-	// first, boot up service A, which depends on B
-	go func() {
-		yamlPath := "../tests/dependencytesting/A.yaml"
-		serviceDefinition, err := parseServiceDefinitionFromYaml(yamlPath)
-		if err != nil {
-			t.Error(err)
-		}
-
-		_, err = registerService(serviceDefinition, sysmanDetails)
-		if err != nil {
-			t.Error(err)
-		}
-		done <- true
-	}()
-
-	// wait 10 seconds, then boot up service B
-	// this should also complete the registration of service A
-	go func() {
-		time.Sleep(10 * time.Second)
-
-		yamlPath := "../tests/dependencytesting/B.yaml"
-		serviceDefinition, err := parseServiceDefinitionFromYaml(yamlPath)
-		if err != nil {
-			t.Error(err)
-		}
-
-		_, err = registerService(serviceDefinition, sysmanDetails)
-		if err != nil {
-			t.Error(err)
-		}
-		done <- true
-	}()
-
-	// wait for both services to register
-	<-done
-	<-done
 }
